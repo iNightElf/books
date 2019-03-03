31a03e0b558a3a11e9509e62eb6730a58f15e41a
iNightElf <mshahzadreza@gmail.com>
2019-03-03 12:57:28 +0600

    Updates

---
+++ precache-manifest.380b75cfaad352920e87723d73587737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddcbc663c850920bc885ad96b74f17ae",
+    "url": "/books/static/media/loading.ddcbc663.gif"
+  },
+  {
+    "revision": "f87e89041d8b8eae3928",
+    "url": "/books/static/js/runtime~main.f87e8904.js"
+  },
+  {
+    "revision": "267b2e5bbae0929bde4a",
+    "url": "/books/static/js/main.267b2e5b.chunk.js"
+  },
+  {
+    "revision": "6e0dca5c622b27e1ff09",
+    "url": "/books/static/js/2.6e0dca5c.chunk.js"
+  },
+  {
+    "revision": "267b2e5bbae0929bde4a",
+    "url": "/books/static/css/main.97561321.chunk.css"
+  },
+  {
+    "revision": "ba4449a718f97f307de6ad107f17864e",
+    "url": "/books/index.html"
+  }
+];
